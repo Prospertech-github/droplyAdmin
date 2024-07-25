983899d98d7990cef65a2d0535962568d60ffd96
henzyd <henzydee@gmail.com>
2024-07-25 13:33:41 +0100

    formatted

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,90 +1,96 @@
 type User = {
-  id: string,
-  account_type: "rider" | "organization",
-  first_name: string,
-  last_name: string,
-  email: string,
-  phone: string,
-  image_url: string,
-  date_joined: string,
-  org_data: Organization,
-}
+  id: string;
+  account_type: "rider" | "organization";
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone: string;
+  image_url: string;
+  date_joined: string;
+  org_data: Organization;
+};
 
 type Organization = {
-  "id": string,
-  "cac_doc": string,
-  "logo": string,
-  "name": string
-  "description": string
-  "price_per_km": number
-  "additional_km": number
-  "address": string
-  "date_created": string
-  status: "pending" | "approved" | "declined",
-  approved_by?: string,
-  second_approved_by?: string,
+  id: string;
+  cac_doc: string;
+  logo: string;
+  name: string;
+  description: string;
+  price_per_km: number;
+  additional_km: number;
+  address: string;
+  date_created: string;
+  status: "pending" | "approved" | "declined";
+  approved_by?: string;
+  second_approved_by?: string;
   user_info: {
-    first_name: string,
-    last_name: string,
-    email: string,
-    phone: string,
-  }
+    first_name: string;
+    last_name: string;
+    email: string;
+    phone: string;
+  };
 
-  commission: number
-}
+  commission: number;
+};
 
 type Rider = {
-  id: string,
-  user: User,
-  company: Organization,
-  last_known_location?: string,
-  last_known_location_updated_at?: string,
-}
+  id: string;
+  user: User;
+  company: Organization;
+  last_known_location?: string;
+  last_known_location_updated_at?: string;
+};
 
 type Order = {
-  "id": string,
-  "rider_info": Rider
-  "booking_id": string,
-  "name": string,
-  "email": string
-  "phone": string
-  "pickup": string
-  "dropoff": string,
-  "pickup_code": string,
-  "dropoff_code": string
-  "recipient": string
-  "amount": number
-  "item": string
-  "recipient_phone": string
-  "delivery_note"?: string,
-  "created_at": string,
-  "status": "pending" | "confirmed" | "completed" | "in_transit" | "picked" | "cancelled",
-  "confirmed_at"?: string,
-  "completed_at"?: string,
-  "in_transit_at"?: string
-  "picked_at"?: string
-  "provider": "whatsapp" | "mobile_app" | "web_app",
-  "rider": string
-  "user"?: string
-}
+  id: string;
+  rider_info: Rider;
+  booking_id: string;
+  name: string;
+  email: string;
+  phone: string;
+  pickup: string;
+  dropoff: string;
+  pickup_code: string;
+  dropoff_code: string;
+  recipient: string;
+  amount: number;
+  item: string;
+  recipient_phone: string;
+  delivery_note?: string;
+  created_at: string;
+  status:
+    | "pending"
+    | "confirmed"
+    | "completed"
+    | "in_transit"
+    | "picked"
+    | "cancelled";
+  confirmed_at?: string;
+  completed_at?: string;
+  in_transit_at?: string;
+  picked_at?: string;
+  provider: "whatsapp" | "mobile_app" | "web_app";
+  rider: string;
+  user?: string;
+};
 
 type WHistory = {
-  "id": string,
-  "amount": number,
-  "transaction_id": string,
-  "transaction_type": "credit" | "debit",
-  "desc": string,
-  "status": "success" | "failed",
-  "created_at": string,
-}
+  id: string;
+  amount: number;
+  transaction_id: string;
+  transaction_type: "credit" | "debit";
+  desc: string;
+  status: "success" | "failed";
+  created_at: string;
+};
 
 type Paginated<T> = {
-  count: number,
-  results: T[]
-}
+  count: number;
+  results: T[];
+};
 
 type Loan = {
-  id: number,
+  id: number;
   request_type: string;
   type_of_bike?: string;
   num_of_bike?: number;
@@ -93,6 +99,6 @@ type Loan = {
   status: "pending" | "approved" | "declined";
   created_at: string;
   user_data: User & {
-    company: Organization
+    company: Organization;
   };
-}
+};
