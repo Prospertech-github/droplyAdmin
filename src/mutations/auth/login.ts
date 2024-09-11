8f7eccd10e2d9b0f4019223d6cfea087bbabe59f
henzyd <henzydee@gmail.com>
2024-09-11 01:38:04 +0100

    fix: remember me functionality

--- src/mutations/auth/login.ts
+++ src/mutations/auth/login.ts
@@ -1,39 +1,48 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query'
-import axios, { AxiosError, isAxiosError } from 'axios'
-import { API_TOKEN } from '../../app-constants'
-import { toast } from 'react-toastify'
-
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios, { isAxiosError } from "axios";
+import { API_TOKEN } from "@/app-constants";
+import { toast } from "react-toastify";
 
 type Data = {
-  access: string
-  refresh: string
-} & User
-async function login(data: { email: string; password: string }) {
-  const { data: response } = await axios.post<{
-    data: Data
-  }>('auth/login/', data)
+  access: string;
+  refresh: string;
+  rememberMe: boolean;
+} & User;
 
-  return response.data
+async function login(data: {
+  email: string;
+  password: string;
+  rememberMe: boolean;
+}) {
+  const { data: response } = await axios.post<{
+    data: Data;
+  }>("auth/login/", data);
+
+  return { ...response.data, rememberMe: data.rememberMe };
 }
 
 export default function useLogin() {
-  const queryClient = useQueryClient()
+  const queryClient = useQueryClient();
 
-  return useMutation<Data, AxiosError<any>, {
-    email: string
-    password: string
-  }>(login, {
-    onSuccess(data) {
-      localStorage.setItem(API_TOKEN, data.refresh)
-      axios.defaults.headers.common['Authorization'] = 'Bearer ' + data.access
-      queryClient.setQueriesData(['auth/users/me/'], data)
-      toast.success('Login successful!')
+  return useMutation(login, {
+    onSuccess(data: Data) {
+      if (data.rememberMe) {
+        localStorage.setItem(API_TOKEN, data.refresh);
+      } else {
+        sessionStorage.setItem(API_TOKEN, data.refresh);
+      }
+      axios.defaults.headers.common["Authorization"] = "Bearer " + data.access;
+      queryClient.setQueriesData(["auth/users/me/"], data);
+      toast.success("Login successful!");
     },
     onError(e) {
       if (isAxiosError(e)) {
-        const possibleErrorMessage = e.response?.data?.detail || e.response?.data?.error || e.response?.data?.message
-        toast.error(possibleErrorMessage || 'An error occurred')
+        const possibleErrorMessage =
+          e.response?.data?.detail ||
+          e.response?.data?.error ||
+          e.response?.data?.message;
+        toast.error(possibleErrorMessage || "An error occurred");
       }
     },
-  })
+  });
 }
